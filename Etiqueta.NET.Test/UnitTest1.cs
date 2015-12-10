9b6563cd95cd3cda57094926a8d8daf569a843bf
Luar Faria <luar.faria@gmail.com>
2015-12-10 10:59:23 -0200

    added overload to send logo image

--- Etiqueta.NET.Test/UnitTest1.cs
+++ Etiqueta.NET.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Etiqueta.NET.Core;
+using System.Drawing;
 
 namespace Etiqueta.NET.Test
 {
@@ -10,36 +11,27 @@ namespace Etiqueta.NET.Test
         [TestMethod]
         public void TestMethod1()
         {
-            var Etiqueta = new Etiqueta.NET.Core.CorreiosLabel("ME", "0001", "005", "123456");
+            var Etiqueta = new CorreiosLabel("ME", "0001", "005", "123456");
 
             var sender = new Sender("Luar Faria", "QMS 17 casa 2 Cond. Mini chacaras", "sobradinho", "Setor de mansões", "73062708", "Brasilia", "DF");
             var receiver = new Receiver("Luar Faria", "QMS 17 casa 2 Cond. Mini chacaras", "sobradinho", "Setor de mansões", "73062708", "Brasilia", "DF");
 
 
+            Etiqueta.Generate("JH980121092BR", sender, receiver, CorreiosLabel.LabelType.CARTA, @"C:\Users\luar.faria\Documents\logo.png");
+            Etiqueta.Generate("JH980121092BR", sender, receiver, CorreiosLabel.LabelType.SEDEX, @"C:\Users\luar.faria\Documents\logo.png");
+            Etiqueta.Generate("JH980121092BR", sender, receiver, CorreiosLabel.LabelType.PAC, @"C:\Users\luar.faria\Documents\logo.png");
+
+
+            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
             Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.SEDEX);
+            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.CARTA);
+
+            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.CARTA, Image.FromFile(@"C:\Users\luar.faria\Documents\logo.png"));
+            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC, Image.FromFile(@"C:\Users\luar.faria\Documents\logo.png"));
+            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.SEDEX, Image.FromFile(@"C:\Users\luar.faria\Documents\logo.png"));
             var caminho = Etiqueta.ExportPDF();
 
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-            caminho = Etiqueta.ExportPDF();
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-            caminho = Etiqueta.ExportPDF();
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.CARTA);
-            caminho = Etiqueta.ExportPDF();
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.CARTA);
-            caminho = Etiqueta.ExportPDF();
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-            caminho = Etiqueta.ExportPDF();
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-            caminho = Etiqueta.ExportPDF();
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-
-            Etiqueta.Generate("SP0014454045BR", sender, receiver, CorreiosLabel.LabelType.PAC);
-            caminho = Etiqueta.ExportPDF();
+       
 
             Assert.IsTrue(true);
         }
--- Etiqueta.NET/CorreiosLabel.cs
+++ Etiqueta.NET/CorreiosLabel.cs
@@ -78,8 +78,8 @@ namespace Etiqueta.NET.Core
         /// <param name="type">Delivery type</param>
 
         public void Generate(String registryCode, Sender sender, Receiver receiver, LabelType type)
-        {            
-            Bitmap bmp = null;
+        {
+            Bitmap bmp = GetLabelTemplate(type);
             RectangleF cartaoPostagemPoint = new RectangleF(130, 25, 150, 22);
             RectangleF agfPoint = new RectangleF(130, 56, 200, 22);
             RectangleF admCodePoint = new RectangleF(385, 52, 200, 22);
@@ -89,13 +89,6 @@ namespace Etiqueta.NET.Core
             RectangleF codRegistroPoint = new RectangleF(130, 88, 150, 22);
             RectangleF codBarrasRegistroPoint = new RectangleF(80, 111, 360, 81);
             RectangleF codBarrasCepPoint = new RectangleF(65, 330, 160, 103);
-
-            switch (type)
-            {
-                case LabelType.CARTA: bmp = Properties.Resources.ModeloEtiquetaCarta; break;
-                case LabelType.PAC: bmp = Properties.Resources.ModeloEtiquetaPAC; break;
-                case LabelType.SEDEX: bmp = Properties.Resources.ModeloEtiquetaSedex; break;
-            }
 
 
             Graphics g = Graphics.FromImage(bmp);
@@ -111,14 +104,139 @@ namespace Etiqueta.NET.Core
             var imgCodigoBarrasZip = GerarCodBarrasCep(receiver.ZipCode);
             g.DrawImage(imgCodigoBarrasZip, codBarrasCepPoint);
 
-            if(type == LabelType.PAC || type == LabelType.SEDEX)
+            if (type != LabelType.CARTA)
             {
                 g.DrawString(admCode, new Font("Arial", 7, FontStyle.Regular), Brushes.Black, admCodePoint);
                 g.DrawString(company, new Font("Arial", 10, FontStyle.Bold), Brushes.Black, companyCodePoint);
-            }            
+                g.DrawString(postCard, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, cartaoPostagemPoint);
+                g.DrawString(agf, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, agfPoint);
+            }      
+          
+            g.DrawString(registryCode, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, codRegistroPoint);            
+            g.DrawString(String.Format(format, receiver.Name.ToUpper(), receiver.Address.ToUpper(), receiver.Complement.ToUpper(), receiver.District.ToUpper(), receiver.ZipCode.ToUpper(), receiver.City.ToUpper(), receiver.State.ToUpper()), new Font("Arial", 10), Brushes.Black, receiverPoint);
+            g.DrawString(String.Format(format, sender.Name.ToUpper(), sender.Address.ToUpper(), sender.Complement.ToUpper(), sender.District.ToUpper(), sender.ZipCode.ToUpper(), sender.City.ToUpper(), sender.State.ToUpper()), new Font("Arial", 10), Brushes.Black, senderPoint);
+            String fileName = "label-" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss-fff") + ".jpg";
+            g.Flush();
+            String path = fileName;
+            bmp.Save(path);
+            bmp.Dispose();
+
+            if (PathsList == null)
+            {
+                PathsList = new List<String>();
+            }
+            PathsList.Add(path);
+        }
+
+        /// <summary>
+        /// Method to generate labels
+        /// </summary>
+        /// <param name="registryCode">Object posted number</param>
+        /// <param name="sender">Sender of package</param>
+        /// <param name="receiver">Receiver of package</param>
+        /// <param name="type">Delivery type</param>
+        /// <param name="logo">Logo size 100x50</param>
+        public void Generate(String registryCode, Sender sender, Receiver receiver, LabelType type, Image logo)
+        {
+            Bitmap bmp = GetLabelTemplate(type);
+            RectangleF logoPoint = new RectangleF(25, 25, 150, 50);
+            RectangleF cartaoPostagemPoint = new RectangleF(130, 25, 150, 22);
+            RectangleF agfPoint = new RectangleF(130, 56, 200, 22);
+            RectangleF admCodePoint = new RectangleF(385, 52, 200, 22);
+            RectangleF companyCodePoint = new RectangleF(390, 62, 200, 22);
+            RectangleF receiverPoint = new RectangleF(20, 260, 380, 80);
+            RectangleF senderPoint = new RectangleF(20, 460, 380, 80);
+            RectangleF codRegistroPoint = new RectangleF(130, 88, 150, 22);
+            RectangleF codBarrasRegistroPoint = new RectangleF(80, 111, 360, 81);
+            RectangleF codBarrasCepPoint = new RectangleF(65, 330, 160, 103);
+
+    
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            String format = "{0}\n{1}\n{2}, {3}\n{4}  {5} - {6}";
+
+            var imgBarCodeRegistry = GerarCodBarrasRegistro(registryCode);
+            g.DrawImage(imgBarCodeRegistry, codBarrasRegistroPoint);
+
+            var imgCodigoBarrasZip = GerarCodBarrasCep(receiver.ZipCode);
+            g.DrawImage(imgCodigoBarrasZip, codBarrasCepPoint);
+
+            if (type != LabelType.CARTA)
+            {
+                g.DrawString(admCode, new Font("Arial", 7, FontStyle.Regular), Brushes.Black, admCodePoint);
+                g.DrawString(company, new Font("Arial", 10, FontStyle.Bold), Brushes.Black, companyCodePoint);
+                g.DrawString(postCard, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, cartaoPostagemPoint);
+                g.DrawString(agf, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, agfPoint);
+            }      
+
+            g.DrawImage(logo, logoPoint);
+            g.DrawString(registryCode, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, codRegistroPoint);            
+            g.DrawString(String.Format(format, receiver.Name.ToUpper(), receiver.Address.ToUpper(), receiver.Complement.ToUpper(), receiver.District.ToUpper(), receiver.ZipCode.ToUpper(), receiver.City.ToUpper(), receiver.State.ToUpper()), new Font("Arial", 10), Brushes.Black, receiverPoint);
+            g.DrawString(String.Format(format, sender.Name.ToUpper(), sender.Address.ToUpper(), sender.Complement.ToUpper(), sender.District.ToUpper(), sender.ZipCode.ToUpper(), sender.City.ToUpper(), sender.State.ToUpper()), new Font("Arial", 10), Brushes.Black, senderPoint);
+            String fileName = "label-" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss-fff") + ".jpg";
+            g.Flush();
+            String path = fileName;
+            bmp.Save(path);
+            bmp.Dispose();
+
+            if (PathsList == null)
+            {
+                PathsList = new List<String>();
+            }
+            PathsList.Add(path);
+        }
+
+        /// <summary>
+        /// Method to generate labels
+        /// </summary>
+        /// <param name="registryCode">Object posted number</param>
+        /// <param name="sender">Sender of package</param>
+        /// <param name="receiver">Receiver of package</param>
+        /// <param name="type">Delivery type</param>
+        /// <param name="logo">Logo size 100x50</param>
+        public void Generate(String registryCode, Sender sender, Receiver receiver, LabelType type, String logoPath)
+        {
+            Bitmap bmp = GetLabelTemplate(type);
+            RectangleF logoPoint = new RectangleF(25, 25, 100, 50);
+            RectangleF cartaoPostagemPoint = new RectangleF(130, 25, 150, 22);
+            RectangleF agfPoint = new RectangleF(130, 56, 200, 22);
+            RectangleF admCodePoint = new RectangleF(385, 52, 200, 22);
+            RectangleF companyCodePoint = new RectangleF(390, 62, 200, 22);
+            RectangleF receiverPoint = new RectangleF(20, 260, 380, 80);
+            RectangleF senderPoint = new RectangleF(20, 460, 380, 80);
+            RectangleF codRegistroPoint = new RectangleF(130, 88, 150, 22);
+            RectangleF codBarrasRegistroPoint = new RectangleF(80, 111, 360, 81);
+            RectangleF codBarrasCepPoint = new RectangleF(65, 330, 160, 103);           
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            String format = "{0}\n{1}\n{2}, {3}\n{4}  {5} - {6}";
+
+            var imgBarCodeRegistry = GerarCodBarrasRegistro(registryCode);
+            g.DrawImage(imgBarCodeRegistry, codBarrasRegistroPoint);
+
+            var imgCodigoBarrasZip = GerarCodBarrasCep(receiver.ZipCode);
+            g.DrawImage(imgCodigoBarrasZip, codBarrasCepPoint);
+
+            if(type != LabelType.CARTA)
+            {
+                g.DrawString(admCode, new Font("Arial", 7, FontStyle.Regular), Brushes.Black, admCodePoint);
+                g.DrawString(company, new Font("Arial", 10, FontStyle.Bold), Brushes.Black, companyCodePoint);
+                g.DrawString(postCard, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, cartaoPostagemPoint);
+                g.DrawString(agf, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, agfPoint);
+            }
+      
+
+            var logo = Image.FromFile(logoPath);                                  
+            g.DrawImage(logo, logoPoint);
             g.DrawString(registryCode, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, codRegistroPoint);
-            g.DrawString(postCard, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, cartaoPostagemPoint);
-            g.DrawString(agf, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, agfPoint);
+            
             g.DrawString(String.Format(format, receiver.Name.ToUpper(), receiver.Address.ToUpper(), receiver.Complement.ToUpper(), receiver.District.ToUpper(), receiver.ZipCode.ToUpper(), receiver.City.ToUpper(), receiver.State.ToUpper()), new Font("Arial", 10), Brushes.Black, receiverPoint);
             g.DrawString(String.Format(format, sender.Name.ToUpper(), sender.Address.ToUpper(), sender.Complement.ToUpper(), sender.District.ToUpper(), sender.ZipCode.ToUpper(), sender.City.ToUpper(), sender.State.ToUpper()), new Font("Arial", 10), Brushes.Black, senderPoint);
             String fileName = "label-" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss-fff") + ".jpg";
@@ -176,6 +294,18 @@ namespace Etiqueta.NET.Core
         #endregion
 
         #region Private Methods
+
+        private Bitmap GetLabelTemplate(LabelType type)
+        {
+            Bitmap bmp = null;
+            switch (type)
+            {
+                case LabelType.CARTA: bmp = Properties.Resources.ModeloEtiquetaCarta; break;
+                case LabelType.PAC: bmp = Properties.Resources.ModeloEtiquetaPAC; break;
+                case LabelType.SEDEX: bmp = Properties.Resources.ModeloEtiquetaSedex; break;
+            }
+            return bmp;
+        }
 
         private Image GerarCodBarrasRegistro(String registro)
         {
